3621be26abc5cc9580974ea83f0d42a96846d42b
mumitprottoy <mumitprottoy@gmail.com>
2023-01-22 01:24:03 +0600

    added two models - Family and Member

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Family(models.Model):
+    name = models.CharField(max_length=25, blank=False, null=False)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Families'
 
 
 
+
+class Member(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+
+    # privileges
+    admin = models.BooleanField(default=False)
+
+    
