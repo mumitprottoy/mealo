814f28d63abb94dd13d42f6ebfb7b7e5fe773470
Mumit Prottoy <mumitprottoy@gmail.com>
2023-02-18 16:17:35 +0600

    Update decorators.py

--- subroutines/decorators.py
+++ subroutines/decorators.py
@@ -3,6 +3,8 @@ from django.shortcuts import redirect
 def auth_check(view):
     def check(request):
         if request.user.is_authenticated:
-            return redirect(str(request.META.get('HTTP_REFERER', 'landing')))
+            url = str(request.META.get('HTTP_REFERER', 'landing'))
+            if 'login' in url: url='landing'
+            return redirect(url)
         else: return view(request)
     return check
